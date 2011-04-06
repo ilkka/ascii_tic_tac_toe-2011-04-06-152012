44baac104dc3e25ea2d52bba2b2a04514887f576
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2011-04-06 15:42:59 +0300

    Stub move()

--- lib/ascii_tic_tac_toe.rb
+++ lib/ascii_tic_tac_toe.rb
@@ -22,4 +22,9 @@ class AsciiTicTacToe
     raise ColumnIndexOutOfBounds unless (column >= 1 and column <= 3)
     @board[row-1][column-1]
   end
+
+  def move(row, column, piece)
+    raise RowIndexOutOfBounds unless (row >= 1 and row <= 3)
+    raise ColumnIndexOutOfBounds unless (column >= 1 and column <= 3)
+  end
 end
