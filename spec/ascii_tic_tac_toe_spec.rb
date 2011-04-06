af33549ae335b225e7d115a96a3540d9d76915f6
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2011-04-06 15:24:01 +0300

    Specs for rows

--- spec/ascii_tic_tac_toe_spec.rb
+++ spec/ascii_tic_tac_toe_spec.rb
@@ -9,4 +9,21 @@ describe AsciiTicTacToe do
       }.should_not raise_exception
     end
   end
+
+  it 'should have 3 rows' do
+    lambda {
+      AsciiTicTacToe.new.row(1)
+      AsciiTicTacToe.new.row(2)
+      AsciiTicTacToe.new.row(3)
+    }.should_not raise_exception
+    lambda {
+      AsciiTicTacToe.new.row(0)
+    }.should_not raise_exception
+    lambda {
+      AsciiTicTacToe.new.row(4)
+    }.should_not raise_exception
+    lambda {
+      AsciiTicTacToe.new.row(-1)
+    }.should_not raise_exception
+  end
 end
