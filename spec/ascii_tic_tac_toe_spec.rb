8c6f4b953ba1eb56656769f26d3a342ecee0aef3
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2011-04-06 15:33:21 +0300

    Refactor specs, replace row, column with at()

--- spec/ascii_tic_tac_toe_spec.rb
+++ spec/ascii_tic_tac_toe_spec.rb
@@ -10,37 +10,15 @@ describe AsciiTicTacToe do
     end
   end
 
-  it 'should have 3 rows' do
-    lambda {
-      AsciiTicTacToe.new.row(1)
-      AsciiTicTacToe.new.row(2)
-      AsciiTicTacToe.new.row(3)
-    }.should_not raise_exception
-    lambda {
-      AsciiTicTacToe.new.row(0)
-    }.should raise_exception
-    lambda {
-      AsciiTicTacToe.new.row(4)
-    }.should raise_exception
-    lambda {
-      AsciiTicTacToe.new.row(-1)
-    }.should raise_exception
-  end
-
-  it 'should have 3 columns' do
-    lambda {
-      AsciiTicTacToe.new.col(1)
-      AsciiTicTacToe.new.col(2)
-      AsciiTicTacToe.new.col(3)
-    }.should_not raise_exception
-    lambda {
-      AsciiTicTacToe.new.col(0)
-    }.should raise_exception
-    lambda {
-      AsciiTicTacToe.new.col(4)
-    }.should raise_exception
-    lambda {
-      AsciiTicTacToe.new.col(-1)
-    }.should raise_exception
+  describe 'at' do
+    it 'should throw an exception for indexes not in 1..3 inclusive' do
+      lambda {
+        AsciiTicTacToe.new.at(1,1)
+        AsciiTicTacToe.new.at(2,2)
+        AsciiTicTacToe.new.at(3,3)
+        AsciiTicTacToe.new.at(1,3)
+        AsciiTicTacToe.new.at(3,1)
+      }.should_not raise_exception
+    end
   end
 end
