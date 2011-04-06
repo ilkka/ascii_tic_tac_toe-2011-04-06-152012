196468bb8d2cbc276239066882e345b58c0d22fc
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2011-04-06 15:29:51 +0300

    Spec for columns, like rows

--- spec/ascii_tic_tac_toe_spec.rb
+++ spec/ascii_tic_tac_toe_spec.rb
@@ -26,4 +26,21 @@ describe AsciiTicTacToe do
       AsciiTicTacToe.new.row(-1)
     }.should raise_exception
   end
+
+  it 'should have 3 columns' do
+    lambda {
+      AsciiTicTacToe.new.col(1)
+      AsciiTicTacToe.new.col(2)
+      AsciiTicTacToe.new.col(3)
+    }.should_not raise_exception
+    lambda {
+      AsciiTicTacToe.new.col(0)
+    }.should raise_exception
+    lambda {
+      AsciiTicTacToe.new.col(4)
+    }.should raise_exception
+    lambda {
+      AsciiTicTacToe.new.col(-1)
+    }.should raise_exception
+  end
 end
