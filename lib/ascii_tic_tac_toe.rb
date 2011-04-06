739ee4a67f463d0d0557bb8929b756ebe98cacc2
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2011-04-06 15:29:15 +0300

    Implement row accessing

--- lib/ascii_tic_tac_toe.rb
+++ lib/ascii_tic_tac_toe.rb
@@ -1,2 +1,20 @@
+class RowIndexOutOfBounds < Exception
+end
+
 class AsciiTicTacToe
+  def empty
+    ' '
+  end
+
+  def initialize
+    @board = [
+      [empty, empty, empty],
+      [empty, empty, empty],
+      [empty, empty, empty],
+    ]
+  end
+
+  def row(index)
+    raise RowIndexOutOfBounds unless (index >= 1 and index <= 3)
+  end
 end
